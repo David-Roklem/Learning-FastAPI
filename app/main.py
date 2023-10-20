6f93c939b09a392ebbe4c0e5f38ddadee56db7a0
David-Roklem <roklem@mail.ru>
2023-10-20 11:55:45 +0300

    Add post_feedback handler

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-# from models.basemodel import User
+from models.basemodel import User, Feedback
 
 app = FastAPI()
 
@@ -9,7 +9,20 @@ fake_users = {
     2: {"username": "jane_smith", "email": "jane@example.com"},
 }
 
+feedbacks = []
+
 
 @app.get("/users/{user_id}")
 def read_user(user_id: int):
     return fake_users.get(user_id, {"error": "User not found"})
+
+
+@app.post("/feedback")
+def post_feedback(feedback: Feedback):
+    name = feedback.name
+    message = feedback.message
+    feedbacks.append(dict(name=name, message=message))
+    with open('file.txt', 'a', encoding='utf-8') as f:
+        f.write(str(feedback) + '\n')
+    answer = f'Feedback received. Thank you, {name}!'
+    return {'message': answer}
